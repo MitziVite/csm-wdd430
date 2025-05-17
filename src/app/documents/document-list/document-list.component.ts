8beb0467a2a49207d34e9fa93152be7df476908c
Mitzi Vite <vit20001@byui.edu>
2025-05-17 15:17:13 -0600

    3/4 4 assignment

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document-model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
-export class DocumentListComponent {
 
+export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document(1, "Document 1", "Description for Document 1", "https://example.com/doc1", []),
+    new Document(2, "Document 2", "Description for Document 2", "https://example.com/doc2", []),
+    new Document(3, "Document 3", "Description for Document 3", "https://example.com/doc3", []),
+    new Document(4, "Document 4", "Description for Document 4", "https://example.com/doc4", []),
+    new Document(5, "Document 5", "Description for Document 5", "https://example.com/doc5", [])
+  ];
+
+  constructor() { }
+  ngOnInit() {}
+
+ 
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
 }
+
+
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-documents',
   standalone: false,
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './documents.component.css'
 })
 export class DocumentsComponent {
-
+  selectedDocument: Document;
 }
